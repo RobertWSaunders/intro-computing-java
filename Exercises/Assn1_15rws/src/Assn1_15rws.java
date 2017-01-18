27ff0134fd2ad2ec7fff48fac3b1335bf37b7ccd
Robert Saunders <robertwsaunders@outlook.com>
2017-01-17 19:28:33 -0500

    Added Assignment 1 - Game of Pigs

---
+++ Exercises/Assn1_15rws/src/Assn1_15rws.java
@@ -0,0 +1,19 @@
+import java.util.Random
+
+/**
+ * Created by robertsaunders on 2017-01-17.
+ */
+
+//single letter entry use byte array
+    //make scanner class attribute
+
+public class Assn1_15rws {
+    int turnScore;
+    int gameScore;
+
+    public static Random generator = new Random(System.currentTimeMillis());
+
+    public static void main(String[] args) {
+
+    }
+}
