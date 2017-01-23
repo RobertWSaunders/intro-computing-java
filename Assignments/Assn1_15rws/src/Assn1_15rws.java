2bca2a4ccf9bd70a35cb99ceff77f785fa7820e8
Robert Saunders <robertwsaunders@outlook.com>
2017-01-23 13:45:57 -0500

    Assignment #1 Progress

--- Assignments/Assn1_15rws/src/Assn1_15rws.java
+++ Assignments/Assn1_15rws/src/Assn1_15rws.java
@@ -10,6 +10,7 @@
 import java.util.Random;
 import java.util.Scanner;
 import java.io.IOException;
+import java.util.stream.*;
 import java.util.concurrent.TimeUnit;
 import java.io.InterruptedIOException;
 
@@ -29,6 +30,14 @@ public class Assn1_15rws {
         computer
     }
 
+    //define the different type of turns
+    private enum DiceCombination {
+        snakeeyes,
+        singleone,
+        matching,
+        random
+    }
+
     //make scanner class attribute, passing in the system input stream to scanner
     private static Scanner scanner = new Scanner(System.in);
     //create flag to keep track of whose turn it is, by default player gets first turn
@@ -40,31 +49,89 @@ public class Assn1_15rws {
     //define counter for computers game sum
     private static int computerSum = 0;
 
+
+
     /**
      * Main method of the program.
      * @param args
      */
     public static void main(String[] args) {
-
+        // play the game as log as no has won the game
+        while (!gameComplete()) {
+            initiateTurn();
+        }
+        //print the winner
+        printWinner();
     }
 
+    public static void initiateTurn() {
+        //print whose tur it is
+        printWhoseTurn();
+        int rollSum = 0;
+        do {
+            int[] rolls = collectRolls();
+            //if the rolls are good then give the user the option to roll again
+            printRoll(rolls);
+            switch (validateRolls(rolls, rollSum)) {
+                case random:
+                    rollSum += IntStream.of(rolls).sum();
+                    addToGameSum(rollSum);
+                    printTurnSum(rollSum);
+                    break;
+                case snakeeyes:
+                    printTurnSummary();
+                    switchTurns();
+                    break;
+                case matching:
+                    //skip roll again, automatically gets rolled again
+                    break;
+                case singleone:
+                    //turn over and turn sum set to zero
 
-    //Printing Methods
+                    break;
+            }
+        } while (rollAgain());
+    }
+
+    /* PRINTING METHODS */
+
+    public static void printWinner() {
+        if (computerSum > playerSum) {
+            System.out.print("Computer Wins!");
+        }
+        else {
+            System.out.print("Player Wins!");
+        }
+    }
 
     public static void printRoll(int ... rolls) {
+        StringBuilder stringBuilder = new StringBuilder();
         switch (turn) {
             case player:
-                System.out.print("Player rolled ");
+                stringBuilder.append("Player rolled ");
                 break;
             case computer:
-                System.out.print("Computer rolled ");
+                stringBuilder.append("Computer rolled ");
                 break;
         }
-        String separator = "";
+        String seperator = "";
         for (int roll : rolls) {
-            System.out.print(separator);
-            System.out.printf("%s", numToString(roll));
-            separator = " + ";
+            stringBuilder.append(seperator);
+            stringBuilder.append(String.format("%s", numToString(roll)));
+            seperator = " + ";
+        }
+        stringBuilder.append("\n");
+        System.out.print(stringBuilder.toString());
+    }
+
+    public static void printWhoseTurn() {
+        switch (turn) {
+            case player:
+                System.out.printf("Player's turn:\n",turn);
+                break;
+            case computer:
+                System.out.printf("Computer's turn:\n",turn);
+                break;
         }
     }
 
@@ -72,10 +139,10 @@ public class Assn1_15rws {
     public static void printTurnSum(int turnSum) {
         switch (turn) {
             case player:
-                System.out.printf("Player's turn sum is: %d and game sum is: %d.",turnSum,playerSum);
+                System.out.printf("Player's turn sum is: %d and game sum is: %d.\n",turnSum,playerSum);
                 break;
             case computer:
-                System.out.printf("Computers's turn sum is: %d and game sum is: %d.",turnSum,computerSum);
+                System.out.printf("Computers's turn sum is: %d and game sum is: %d.\n",turnSum,computerSum);
                 break;
         }
     }
@@ -90,11 +157,34 @@ public class Assn1_15rws {
 
     }
 
-    //Checks
 
     //prompts player to see if wants to roll
     public static boolean rollAgain() {
-        return true;
+        //create array of type byte, holds one bytes, only looking for one response
+        byte[] buffer = new byte[1];
+        //declare integer to count how many characters were entered
+        int numRead = -1;
+        //ask the user to type something
+        System.out.print("Roll again? (y/n)");
+        //try catch block to handle exception
+        try {
+            //returns number of bytes in buffer
+            numRead = System.in.read(buffer);
+        } catch (IOException e) {
+            //output to user if an exception is caught
+            System.out.println("Should not have got here!");
+        }
+
+        if ((char) buffer[0] == 'y') {
+            return true;
+        }
+        else if ((char) buffer[0] == 'n') {
+            turn = Turn.computer;
+            return false;
+        }
+        else {
+            return false;
+        }
     }
 
     public static boolean gameComplete() {
@@ -107,7 +197,70 @@ public class Assn1_15rws {
         return false;
     }
 
-    //Utility Methods
+    /* UTILITY METHODS */
+
+    //where validation of turn happens
+    public static DiceCombination validateRolls(int[] rolls, int rollSum) {
+        //checks first case, are all rolls the same
+        for(int i=1; i<rolls.length; i++){
+            if((rolls[0] == rolls[i]) && (rolls[0] == 1)){
+                resetGameSum();
+                return DiceCombination.snakeeyes;
+            }
+        }
+        //checks second case, if any rolls equal one
+        for(int roll : rolls){
+            if(roll == 1){
+                rollSum = 0;
+                return DiceCombination.singleone;
+            }
+        }
+
+        //checks third case, if rolls all match
+        for(int i=1; i<rolls.length; i++){
+            if(rolls[0] == rolls[i]){
+                resetGameSum();
+                return DiceCombination.matching;;
+            }
+        }
+        return DiceCombination.random;
+    }
+
+    public static void switchTurns() {
+        switch (turn) {
+            case player:
+                turn = Turn.computer;
+                break;
+            case computer:
+                turn = Turn.player;
+                break;
+        }
+    }
+
+
+    public static void addToGameSum(int amountAdded) {
+        switch (turn) {
+            case player:
+                playerSum += amountAdded;
+                break;
+            case computer:
+                computerSum += amountAdded;
+                break;
+        }
+    }
+
+    public static void resetGameSum() {
+        switch (turn) {
+            case player:
+                System.out.print("Setting Player's game sum to zero!\n");
+                playerSum = 0;
+                break;
+            case computer:
+                System.out.print("Setting Computer's game sum to zero!\n");
+                computerSum = 0;
+                break;
+        }
+    }
 
     /**
      * Converts an int to its english word (string) equivalent, compatible for 0 to 6 inclusive.
@@ -137,7 +290,7 @@ public class Assn1_15rws {
         }
     }
 
-    //Dice Rolling Methods
+    /* DICE ROLLING METHODS */
 
     /**
      * Rolls a virtual dice and returns the string equivalent.
@@ -149,5 +302,18 @@ public class Assn1_15rws {
         Random generator = new Random(System.currentTimeMillis());
         //the string of the random number
         return generator.nextInt(DICE_SIDES+1);
+    }
+
+    /**
+     * Will collect n (represented by NUMBER_ROLLS_PER_TURN) rolls and return an array full of the
+     * integers that represent a roll.
+     * @return An array with n (represented by NUMBER_ROLLS_PER_TURN) integers that represent a dice roll
+     */
+    public static int[] collectRolls() {
+        int[] rolls = new int[NUMBER_ROLLS_PER_TURN];
+        for (int i = 0; i < rolls.length; i++) {
+            rolls[i] = rollDice();
+        }
+        return rolls;
     }
 }
